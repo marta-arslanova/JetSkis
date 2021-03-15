131e42c71c6b336e8ae778ad4a7f1e23102b7254
Marta Arslanova <marta.arslanova@icloud.com>
2021-03-16 01:43:09 +0300

    Сверстана шапка сайта и секция с баннером

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/banner-section/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/banner-section/arrow-right.svg" alt="arrow-right"></button>',
+  });
+
+});
