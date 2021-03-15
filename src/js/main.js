6cf5527e0df2f8755e237201995302fdf6b812e9
Marta Arslanova <marta.arslanova@icloud.com>
2021-03-16 02:30:48 +0300

    Верстка табов с поиском

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,14 @@ $(function(){
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/banner-section/arrow-right.svg" alt="arrow-right"></button>',
   });
 
+  $('.search__tabs-item').on('click', function(e) {
+    e.preventDefault();
+
+    $('.search__tabs-item').removeClass('search__tabs-item--active');
+    $('.search__content-item').removeClass('search__content-item--active');
+
+    $(this).addClass('search__tabs-item--active');
+    $($(this).attr('href')).addClass('search__content-item--active');
+  });
+
 });
